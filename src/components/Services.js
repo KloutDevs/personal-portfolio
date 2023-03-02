952aa8e03b79daa055f9a3dc69db033702ef8680
Klout <kloutdevs@gmail.com>
2023-03-02 05:47:55 -0300

    Pages updated

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,16 +1,20 @@
-import ColorLens from '@mui/icons-material/ColorLens';
+/* REQUIRED IMPORTS */
+
+//import ColorLens from '@mui/icons-material/ColorLens'; This is for the Designs Item
 import DataObject from '@mui/icons-material/DataObject';
 import StorageIcon from '@mui/icons-material/Storage';
 import CloseIcon from '@mui/icons-material/Close';
-import { IconButton ,Button, Typography, Dialog, DialogTitle, DialogContent, DialogActions } from '@mui/material';
+import { IconButton, Button, Typography, Dialog, DialogTitle, DialogContent, DialogActions } from '@mui/material';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import Paper from '@mui/material/Paper';
 import { styled } from '@mui/material/styles';
 import * as React from 'react';
-import Count from 'react-countup';
+import Count from 'react-countup'; // The Counter Component
 import CustomTheme from '../utils/theme';
 import PropTypes from 'prop-types';
+
+/* STYLES */
 
 const Item = styled(Paper)(({ theme }) => ({
     backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
@@ -20,14 +24,21 @@ const Item = styled(Paper)(({ theme }) => ({
 
     color: theme.palette.text.secondary,
 }));
-
+const Modal = styled(Dialog)(({ theme }) => ({
+    '& .MuiDialogContent-root': {
+        padding: theme.spacing(2),
+    },
+    '& .MuiDialogActions-root': {
+        padding: theme.spacing(1),
+    },
+}));
 const styles = {
     list: {
         width: '100%',
         maxWidth: 360,
         bgcolor: 'background.paper'
     },
-    buttomKnowMore: {
+    buttom: {
         bgcolor: CustomTheme.palette.primary.main,
         '&:hover': {
             bgcolor: CustomTheme.palette.primary.dark
@@ -35,55 +46,90 @@ const styles = {
     },
 
 }
-
-const BootstrapDialog = styled(Dialog)(({ theme }) => ({
-    '& .MuiDialogContent-root': {
-      padding: theme.spacing(2),
-    },
-    '& .MuiDialogActions-root': {
-      padding: theme.spacing(1),
-    },
-  }));
-  
-  function BootstrapDialogTitle(props) {
+function ModalTitle(props) {
     const { children, onClose, ...other } = props;
-  
+
     return (
-      <DialogTitle sx={{ m: 0, p: 2 }} {...other}>
-        {children}
-        {onClose ? (
-          <IconButton
-            aria-label="close"
-            onClick={onClose}
-            sx={{
-              position: 'absolute',
-              right: 8,
-              top: 8,
-              color: (theme) => theme.palette.grey[500],
-            }}
-          >
-            <CloseIcon />
-          </IconButton>
-        ) : null}
-      </DialogTitle>
+        <DialogTitle sx={{ m: 0, p: 2 }} {...other}>
+            {children}
+            {onClose ? (
+                <IconButton
+                    aria-label="close"
+                    onClick={onClose}
+                    sx={{
+                        position: 'absolute',
+                        right: 8,
+                        top: 8,
+                        color: (theme) => theme.palette.grey[500],
+                    }}
+                >
+                    <CloseIcon />
+                </IconButton>
+            ) : null}
+        </DialogTitle>
     );
-  }
-  
-  BootstrapDialogTitle.propTypes = {
+}
+ModalTitle.propTypes = {
     children: PropTypes.node,
     onClose: PropTypes.func.isRequired,
-  };
+};
+
+/* COMPONENT */
 
 export default function AutoGrid() {
 
-    const [open, setOpen] = React.useState(false);
+    const [modal, setModal] = React.useState(false);
+    const [modalContent, setModalContent] = React.useState(null);
 
-    const handleClickOpen = () => {
-      setOpen(true);
+    const handleClickOpen = () => { // Handle View More Btn
+        setModal(true);
     };
-    const handleClose = () => {
-      setOpen(false);
+    const handleClose = () => { // Handle Modal on Close
+        setModal(false);
+        setModalContent(null);
     };
+
+    let DialogInfo = { // Dialog Data
+        front: {
+            title: (<ModalTitle id="customized-dialog-title" onClose={handleClose}>
+                Front-End Services
+            </ModalTitle>),
+            body: (<DialogContent dividers>
+                <Typography gutterBottom>
+                    Ipsum id nulla laboris proident aliquip reprehenderit. Dolore culpa commodo et ipsum et occaecat non dolor deserunt sit. Ea excepteur magna esse officia. Mollit reprehenderit sint elit eiusmod magna et consectetur ad.
+                </Typography>
+            </DialogContent>),
+            actions: (<DialogActions>
+                <Button autoFocus onClick={handleClose}>
+                    Contact Me
+                </Button>
+            </DialogActions>)
+        },
+        back: {
+            title: (<ModalTitle id="customized-dialog-title" onClose={handleClose}>
+                Back-End Services
+            </ModalTitle>),
+            body: (<DialogContent dividers>
+                <Typography gutterBottom>
+                    Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
+                    dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
+                    consectetur ac, vestibulum at eros.
+                </Typography>
+            </DialogContent>),
+            actions: (<DialogActions>
+                <Button autoFocus onClick={handleClose}>
+                    Contact Me
+                </Button>
+            </DialogActions>)
+        }
+    }
+
+    const handleModalContent = (props) => { // Define the Modal Content
+        if (DialogInfo[props] !== undefined && DialogInfo[props] !== null) {
+            let content = DialogInfo[props];
+            setModalContent(content);
+        }
+    }
 
     return (
         <Box sx={{ flexGrow: 1 }}>
@@ -116,15 +162,57 @@ export default function AutoGrid() {
                             </Typography>
                         </Grid>
                         <Grid className='service-viewmore'>
-                            <Button sx={styles.buttomKnowMore} variant="contained" onClick={handleClickOpen} >VIEW MORE</Button>
-                            <BootstrapDialog
+                            <Button sx={styles.buttom} variant="contained" onClick={() => { handleClickOpen(); handleModalContent('front') }} >VIEW MORE</Button>
+                        </Grid>
+                    </Item>
+                    <Item className='service-item'>
+                        <Grid className='service-header'>
+                            <Grid className='header-text'>
+                                <Typography component="h4" variant="h3">
+                                    <Count start={0} end={19} duration={1.8} delay={0} /> total services
+                                </Typography>
+                            </Grid>
+                            <Grid className='header-icon'>
+                                <StorageIcon sx={{ color: CustomTheme.palette.primary.main }} />
+                            </Grid>
+                        </Grid>
+                        <Grid className='service-title'>
+                            <Typography component="h3" variant="h2">
+                                Back-End
+                            </Typography>
+                        </Grid>
+                        <Grid className='service-viewmore'>
+                            <Button sx={styles.buttom} variant="contained" onClick={() => { handleClickOpen(); handleModalContent('back') }} >VIEW MORE</Button>
+                        </Grid>
+                    </Item>
+                    
+                    {/* This Item is the Designs Services but is actually off
+                                         <Item className='service-item'>
+                        <Grid className='service-header'>
+                            <Grid className='header-text'>
+                                <Typography component="h4" variant="h3">
+                                    <Count start={0} end={19} duration={1.8} delay={0} /> total services
+                                </Typography>
+                            </Grid>
+                            <Grid className='header-icon'>
+                                <ColorLens sx={{ color: CustomTheme.palette.primary.main }} />
+                            </Grid>
+                        </Grid>
+                        <Grid className='service-title'>
+                            <Typography component="h3" variant="h2">
+                                Designs
+                            </Typography>
+                        </Grid>
+                        <Grid className='service-viewmore'>
+                            <Button sx={styles.buttom} variant="contained">VIEW MORE</Button>
+                            <Modal
                                 onClose={handleClose}
                                 aria-labelledby="customized-dialog-title"
-                                open={open}
+                                open={modal}
                             >
-                                <BootstrapDialogTitle id="customized-dialog-title" onClose={handleClose}>
+                                <ModalTitle id="customized-dialog-title" onClose={handleClose}>
                                     Modal title
-                                </BootstrapDialogTitle>
+                                </ModalTitle>
                                 <DialogContent dividers>
                                     <Typography gutterBottom>
                                         Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
@@ -146,49 +234,19 @@ export default function AutoGrid() {
                                         Save changes
                                     </Button>
                                 </DialogActions>
-                            </BootstrapDialog>
+                            </Modal>
                         </Grid>
-                    </Item>
-                    <Item className='service-item'>
-                        <Grid className='service-header'>
-                            <Grid className='header-text'>
-                                <Typography component="h4" variant="h3">
-                                    <Count start={0} end={19} duration={1.8} delay={0} /> total services
-                                </Typography>
-                            </Grid>
-                            <Grid className='header-icon'>
-                                <StorageIcon sx={{ color: CustomTheme.palette.primary.main }} />
-                            </Grid>
-                        </Grid>
-                        <Grid className='service-title'>
-                            <Typography component="h3" variant="h2">
-                                Back-End
-                            </Typography>
-                        </Grid>
-                        <Grid className='service-viewmore'>
-                            <Button sx={styles.buttomKnowMore} variant="contained" >VIEW MORE</Button>
-                        </Grid>
-                    </Item>
-                    <Item className='service-item'>
-                        <Grid className='service-header'>
-                            <Grid className='header-text'>
-                                <Typography component="h4" variant="h3">
-                                    <Count start={0} end={19} duration={1.8} delay={0} /> total services
-                                </Typography>
-                            </Grid>
-                            <Grid className='header-icon'>
-                                <ColorLens sx={{ color: CustomTheme.palette.primary.main }} />
-                            </Grid>
-                        </Grid>
-                        <Grid className='service-title'>
-                            <Typography component="h3" variant="h2">
-                                Designs
-                            </Typography>
-                        </Grid>
-                        <Grid className='service-viewmore'>
-                            <Button sx={styles.buttomKnowMore} variant="contained">VIEW MORE</Button>
-                        </Grid>
-                    </Item>
+                    </Item> */}
+                    <Modal
+                        id="dialogFront"
+                        onClose={handleClose}
+                        aria-labelledby="customized-dialog-title"
+                        open={modal}
+                    >
+                        {modalContent !== null ? modalContent.title : ''}
+                        {modalContent !== null ? modalContent.body : ''}
+                        {modalContent !== null ? modalContent.actions : ''}
+                    </Modal>
                 </Grid>
             </Grid>
         </Box>
--- src/components/Works.js
+++ src/components/Works.js
@@ -1,12 +1,7 @@
-import * as React from 'react';
+import { CheckCircle, Dangerous, Error } from '@mui/icons-material';
+import { Box, Grid, ImageList, ImageListItem, Paper, Tab, Tabs, ToggleButton, ToggleButtonGroup, Typography } from '@mui/material';
 import { styled } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import Paper from '@mui/material/Paper';
-import Grid from '@mui/material/Grid';
-import { Typography } from '@mui/material';
-import Tabs from './Tabs.tsx';
-import WorksGalery from './WorksGalery.tsx';
-import WorksGaleryFilter from './WorksGaleryFilter.tsx';
+import * as React from 'react';
 
 const Item = styled(Paper)(({ theme }) => ({
     backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
@@ -17,7 +12,123 @@ const Item = styled(Paper)(({ theme }) => ({
     color: theme.palette.text.secondary,
 }));
 
+const itemData = [
+    {
+        img: 'https://images.unsplash.com/photo-1549388604-817d15aa0110',
+        title: 'Bed',
+        category: "works",
+        state: "finalized"
+    },
+    {
+        img: 'https://images.unsplash.com/photo-1525097487452-6278ff080c31',
+        title: 'Books',
+        category: "works",
+        state: "finalized"
+    },
+    {
+        img: 'https://images.unsplash.com/photo-1523413651479-597eb2da0ad6',
+        title: 'Sink',
+        category: "works",
+        state: "finalized"
+    },
+    {
+        img: 'https://images.unsplash.com/photo-1563298723-dcfebaa392e3',
+        title: 'Kitchen',
+        category: "projects",
+        state: "finalized"
+    },
+    {
+        img: 'https://images.unsplash.com/photo-1588436706487-9d55d73a39e3',
+        title: 'Blinds',
+        category: "projects",
+        state: "in-progress"
+    },
+    {
+        img: 'https://images.unsplash.com/photo-1574180045827-681f8a1a9622',
+        title: 'Chairs',
+        category: "projects",
+        state: "in-progress"
+    },
+    {
+        img: 'https://images.unsplash.com/photo-1530731141654-5993c3016c77',
+        title: 'Laptop',
+        category: "projects",
+        state: "in-progress"
+    },
+    {
+        img: 'https://images.unsplash.com/photo-1481277542470-605612bd2d61',
+        title: 'Doors',
+        category: "projects",
+        state: "in-progress"
+    },
+    {
+        img: 'https://images.unsplash.com/photo-1517487881594-2787fef5ebf7',
+        title: 'Coffee',
+        category: "projects",
+        state: "in-progress"
+    },
+    {
+        img: 'https://images.unsplash.com/photo-1516455207990-7a41ce80f7ee',
+        title: 'Storage',
+        category: "projects",
+        state: "not-started"
+    },
+    {
+        img: 'https://images.unsplash.com/photo-1597262975002-c5c3b14bbd62',
+        title: 'Candle',
+        category: "projects",
+        state: "not-started"
+    },
+    {
+        img: 'https://images.unsplash.com/photo-1519710164239-da123dc03ef4',
+        title: 'Coffee table',
+        category: "projects",
+        state: "not-started"
+    },
+];
+
 export default function AutoGrid() {
+
+    const [category, setcategory] = React.useState('one');
+    const [filter, setFilter] = React.useState(null);
+    const [items, setItems] = React.useState(itemData);
+
+    function updateItems(category, filter){
+        let categories = {
+            one: "all",
+            two: "works",
+            three: "projects",
+            four: "organizations"
+        }
+        let newItems = [];
+        items.forEach((item) => {
+            console.log(`I-CATEGORY ${item.category}   S-CATEGORY ${categories[category]}`);
+            console.log(`I-STATE ${item.state}     S-FILTER ${filter}`);
+            if(item.category === categories[category] && filter !== null && item.state === filter){
+                newItems.push(item);
+            }else if(categories[category] === 'all' && filter !== null && item.state === filter){
+                newItems.push(item);
+            }else{
+                newItems = itemData;
+            }
+            console.log('NEW ITEMS');
+            console.log(newItems);
+        });
+        setItems(newItems);
+        console.log('ESTABLISHED ITEMS')
+        console.log(items);
+    }
+
+    const handleCategory = (event, newcategory) => {
+        setcategory(newcategory);
+        updateItems(newcategory, filter);
+    };
+
+    const handleFilter = (event, nextFilter) => {
+        setFilter(nextFilter);
+        updateItems(category, nextFilter);
+    };
+
     return (
         <Box sx={{ flexGrow: 1 }}>
             <Grid id="works-container" container spacing={3}>
@@ -32,14 +143,56 @@ export default function AutoGrid() {
                     </Item>
                 </Grid>
                 <Grid className='options-container' item xs={6}>
-                    <Tabs/>
+                    <Box className='box-tabs' sx={{ width: '100%' }}>
+                        <Tabs
+                            className='tabs-container'
+                            value={category}
+                            onChange={handleCategory}
+                            textColor="secondary"
+                            indicatorColor="primary"
+                        >
+                            <Tab className='worksTab' value="one" label="ALL" />
+                            <Tab className='projectsTab' value="two" label="WORKS" />
+                            <Tab className='orgsTab' value="three" label="PROJECTS" />
+                            <Tab className='orgsTab' value="four" label="ORGANIZATIONS" disabled />
+                        </Tabs>
+                    </Box>
                 </Grid>
                 <Grid className='works-galery' item xs={9}>
                     <Item className='galery-container'>
-                        <WorksGalery/>
+                        <Box className='galery-box' sx={{ width: 900, height: 450, overflowY: 'scroll' }}>
+                            <ImageList variant="masonry" cols={3} gap={8}>
+                                {items.map((item) => (
+                                    <ImageListItem key={item.img}>
+                                        <img
+                                            src={`${item.img}?w=248&fit=crop&auto=format`}
+                                            srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
+                                            alt={item.title}
+                                            loading="lazy"
+                                        />
+                                    </ImageListItem>
+                                ))}
+                            </ImageList>
+                        </Box>
                     </Item>
                     <Item className='galery-filter'>
-                        <WorksGaleryFilter/>
+                        <ToggleButtonGroup
+                            className='worksFilter'
+                            orientation="vertical"
+                            value={filter}
+                            exclusive
+                            onChange={handleFilter}
+                        >
+                            <ToggleButton className='okButton' value="okBtn" aria-label="okBtn">
+                                <CheckCircle />
+                            </ToggleButton>
+                            <ToggleButton className='inProcessButton' value="inProcessButton" aria-label="inProcessButton">
+                                <Error />
+                            </ToggleButton>
+                            <ToggleButton className='notstartedButton' value="notstartedButton" aria-label="notstartedButton">
+                                <Dangerous />
+                            </ToggleButton>
+                        </ToggleButtonGroup>
                     </Item>
                 </Grid>
             </Grid>
